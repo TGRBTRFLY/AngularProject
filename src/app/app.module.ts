e4a2c5d950393e1def89b3f5608de46822341044
PagePetty <pagevpetty@live.com>
2019-06-05 00:08:24 -0600

    10. Adding Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ProductsComponent} from './products.component';
+import {ProductComponent} from './product/product.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +18,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
